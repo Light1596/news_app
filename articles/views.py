a3531780deea87ff616812363e7fed8349e40abf
Light Situma <lightsituma@gmail.com>
2024-12-28 20:09:00 +0000

    first commit

---
+++ articles/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import DeleteView, UpdateView,CreateView
+from django.urls import reverse_lazy
+from .models import Articles
+
+# Create your views here.
+class ArticleListView(ListView):
+    model = Articles
+    template_name = 'article_list.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Articles
+    fields = ['title', 'body']
+    template_name= 'article_edit.html'
+
+class ArticleDetailView(DetailView):
+    model = Articles
+    template_name = 'article_detail.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Articles
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Articles
+    template_name = 'article_new.html'
+    fields = ['title', 'body', 'author']
